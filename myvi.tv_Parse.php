9ce02e7a9a5d4385ddbdcb8b81057d2b6d7b07fc
Msahli <72040730+mouutz@users.noreply.github.com>
2020-12-27 04:16:10 +0100

    Final Version

--- myvi.tv_Parse.php
+++ myvi.tv_Parse.php
@@ -3,7 +3,7 @@
 include('simple_html_dom.php');
 
  
-$vidID = 'pq1iaxsjujpwipwhf6qzn58y6h';
+$vidID = 'pq1iaxsjujpwipwhf6qzn58y6h'; // id video -> https://www.myvi.tv/embed/pq1iaxsjujpwipwhf6qzn58y6h
 
 $source = file_get_contents("https://www.myvi.tv/embed/".$vidID);
 
@@ -15,5 +15,17 @@ $json = urldecode($text);
 
 $url = substr($json, 0, strpos($json, "\u0026"));
 
-echo $url
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$a = curl_exec($ch);
+
+$url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); 
+$realUrl = $url; 
+
+
+echo $realUrl
 ?>
